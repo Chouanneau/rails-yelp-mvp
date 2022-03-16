d961fd83d12551296166b685611c4ae4a69f3501
Matthieu Chouanneau <m.chouanneau@gmail.com>
2022-03-16 14:04:15 +0100

    restaurants

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem "faker"
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form', github: 'heartcombo/simple_form'
+
 ruby '3.0.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+puts "Creating restaurants..."
+5.times do
+restaurant = Restaurant.create!(
+  name: Faker::Name.name,
+  address: Faker::Address.street_name,
+  phone_number: Faker::PhoneNumber.phone_number,
+  category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+)
+end
+puts "Finished!"
